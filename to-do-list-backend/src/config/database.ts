2d309b9f0017d287d4b1fe3f46153d346e80a5ce
Eduardo Beilfuss <eduardobeilfuss8@gmail.com>
2025-07-03 20:46:39 -0300

    adicionando limite de conexões ativas pra usar banco gratuito clever-cloud

--- to-do-list-backend/src/config/database.ts
+++ to-do-list-backend/src/config/database.ts
@@ -9,7 +9,8 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  connectionLimit: 4 
 });
 
 export default db;
